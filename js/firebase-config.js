aeb40d0ad42995abaef38ea051c9088aeadba0a7
sheniseee <s10273890@connect.np.edu.sg>
2026-02-07 02:15:54 +0800

    updates firebase javascript

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,8 @@
-// Import the functions you need from the SDKs you need
-// USING CDN IMPORTS because there is no bundler (Vite/Webpack) in this project.
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyAriRv3BRff1Cv_9he533aseRbPHDqTHRU",
     authDomain: "fed-assignment-66836.firebaseapp.com",
