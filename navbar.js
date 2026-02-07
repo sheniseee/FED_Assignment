500cc897190d4fc171b76846aed55068a3285748
Jiliana Sky <jilianacapili@gmail.com>
2026-02-07 22:51:57 +0800

    Added Mobile Version

--- navbar.js
+++ navbar.js
@@ -1,6 +1,17 @@
-const userToggle = document.querySelector(".user-toggle");
-const userDropdown = document.querySelector(".user-dropdown");
+document.addEventListener('DOMContentLoaded', function () {
+  var userToggle = document.querySelector('.user-toggle');
+  var userDropdown = document.querySelector('.user-dropdown');
+  if (userToggle && userDropdown) {
+    userToggle.addEventListener('click', function () {
+      userDropdown.classList.toggle('open');
+    });
+  }
 
-userToggle.addEventListener("click", () => {
-  userDropdown.classList.toggle("open");
+  var navToggle = document.querySelector('.nav-toggle');
+  var navbar = document.querySelector('.navbar');
+  if (navToggle && navbar) {
+    navToggle.addEventListener('click', function () {
+      navbar.classList.toggle('open');
+    });
+  }
 });
